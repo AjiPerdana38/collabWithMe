035e88932d2198370d4d42d1aa16b6b6b69e3862
ajiaja38 <110799189+ajiaja38@users.noreply.github.com>
2022-08-08 13:37:18 +0700

    Dev1 (#3)
    * Create apatah.js
    
    * Update apatah.js
    
    * Update apatah.js
    
    Co-authored-by: M Aji Perdana <77820694+AjiPerdana38@users.noreply.github.com>

---
+++ apatah.js
@@ -0,0 +1,20 @@
+const mahasiswa = {
+  mhs1{
+    nama : "M Aji Perdana",
+    npm : 19411011
+  },
+  mhs2{
+    nama : "Taufik Azis",
+    npm  : 29411011
+  },
+  mhs3{
+    nama : "Dimas Ramadhan",
+    npm  : 28411011
+  },
+  mhs4{
+    nama : "Ridwan Alamsyah",
+    npm  : 37411011
+  }
+}
+
+console.table(mahasiswa)
